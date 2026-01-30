66c27db063e5838ee83637802da2977cb7e470b7
BACNext-Contributor <bacnext@protonmail.com>
2026-01-30 18:42:27 +0800

    feat: test openapi fetcher

---
+++ scripts/fetch_openapi.py
@@ -0,0 +1,41 @@
+import httpx
+import os
+
+API_KEY = os.environ.get("APIFOX_API_KEY")
+
+moduleIds = [7055935, 7056204, 7056214]
+
+moduleId_to_filename = {
+    7055935: "main.json",
+    7056204: "hyg.json",
+    7056214: "passport.json",
+}
+
+for moduleId in moduleIds:
+    response = httpx.post(
+        "https://api.apifox.com/v1/projects/7776376/export-openapi?locale=zh-CN",
+        headers={
+            "X-Apifox-Api-Version": "2024-03-28",
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type": "application/json",
+        },
+        json={
+            "scope": {
+                "type": "ALL"
+            },
+            "options": {
+                "addFoldersToTags": True
+            },
+            "oasVersion": "3.1",
+            "exportFormat": "JSON",
+            "moduleId": moduleId
+        }
+    )
+    response.raise_for_status()
+    try:
+        with open(f"openapi/{moduleId_to_filename[moduleId]}", "w", encoding="utf-8") as f:
+            f.write(response.text)
+    except Exception as e:
+        print(f"Failed to write file for moduleId {moduleId}: {e}")
+    else:
+        print(f"Successfully wrote file for moduleId {moduleId}")
